285870de970a4f18c0471ff0d4a926554171ffa6
Poonam Bora <165922306+PoonamBora24@users.noreply.github.com>
2025-10-25 22:50:39 +0530

    Update Addtocart.spec.js

--- tests/Addtocart.spec.js
+++ tests/Addtocart.spec.js
@@ -3,4 +3,7 @@ import {test} from "@playwright/test"
 test("Validate Add ti cart", async function({page})
 {
     await page.goto("https://freelance-learn-automation.vercel.app/login");
+    await page.getByText("Manage",{exact:true}).click();
+    await page.getByAltText("manage course").click();
+    await page.locator("//button[text()='Add New Course ']").click();
 })
