adfa9c4370d985639304d03d4cf4c155926b0f9c
Poonam Bora <poonam24.chopda@gmail.com>
2025-10-25 22:51:21 +0530

    modified Add to cart

--- tests/Addtocart.spec.js
+++ tests/Addtocart.spec.js
@@ -3,4 +3,8 @@ import {test} from "@playwright/test"
 test("Validate Add ti cart", async function({page})
 {
     await page.goto("https://freelance-learn-automation.vercel.app/login");
+    await page.getByPlaceholder("Enter Email").pressSequentially("admin@email.com",{delay:200});
+    await page.getByPlaceholder("Enter Password").pressSequentially("admin@123",{delay:200});
+    await page.getByText("Sign in").last().click();
+    
 })
